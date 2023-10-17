e0781c38ea49eaf45b31dd397130810a4c800ee3
Luis nava <lnavaa1500@alumno.ipn.mx>
2023-10-16 20:51:06 -0600

    First commit

---
+++ mrfreaktoy-market/src/test/java/com/mrfreaktoy/market/MrfreaktoyMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrfreaktoy.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrfreaktoyMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
